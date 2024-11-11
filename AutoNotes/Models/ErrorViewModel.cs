cfb6468136b38e7863867ea694f6410f0cba778e
706245-hash <tafara860@gmail.com>
2024-11-11 09:23:04 +0000

    Added to repo

---
+++ AutoNotes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutoNotes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
